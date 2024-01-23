f20dc8462f5339714daef6579915d23ee48f2def
athowes <adamthowes@gmail.com>
2024-01-23 15:28:01 +0000

    App improvements

--- dashboard/app.R
+++ dashboard/app.R
@@ -3,18 +3,26 @@ library(shiny)
 source("functions.R")
 
 ui <- fluidPage(
-  titlePanel("Cost effectiveness analysis for an AMR point-of-care test"),
+  titlePanel("CEA for an AMR point-of-care test"),
   sidebarLayout(
     sidebarPanel(
       numericInput(inputId = "sensitivity",
-                   label = "Sensitivity:",
-                   value = 0.9),
+                        label = "Sensitivity:",
+                        value = 0.9,
+                        min = 0,
+                        max = 1,
+                        step = 0.01),
       numericInput(inputId = "specificity",
-                   label = "Specificity:",
-                   value = 0.95),
+                        label = "Specificity:",
+                        value = 0.95,
+                        min = 0,
+                        max = 1,
+                        step = 0.01),
       numericInput(inputId = "amr_burden_per_abx",
-                   label = "AMR burden per antibiotic:",
-                   value = 0.3)
+                        label = "AMR burden per antibiotic:",
+                        value = 0.3,
+                        min = 0,
+                        step = 0.01)
     ),
     mainPanel(
       htmlOutput(outputId = "ceaOutput")
@@ -26,9 +34,10 @@ server <- function(input, output) {
   output$ceaOutput <- renderText({
     out <- run_cea(input$sensitivity, input$specificity, input$amr_burden_per_abx)
     text <- paste0(
-      "ICER: ", out$icer, "<br>",
-      "Incremental cost ($): ", out$cost_incremental, "<br>",
-      "Incremental benefit (DALYs): ", out$benefit_incremental
+      "Incremental cost: $", round(out$cost_incremental), "<br>",
+      "Incremental benefit: ", round(out$benefit_incremental), " DALYs <br>",
+      "Incremental cost effectiveness ratio: ", round(out$icer, 2), "<br>",
+      "DALYs per dollar: ", round(1 / out$icer, 4), "<br>"
     )
     return(text)
   })
