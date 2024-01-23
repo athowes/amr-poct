cb8df62999a720e7613cdb30c62342e28d8c2272
athowes <adamthowes@gmail.com>
2024-01-23 12:37:48 +0000

    Test shinylive

--- build.R
+++ build.R
@@ -1,1 +1,5 @@
+# Generate notebook
 rmarkdown::render(input = "notebooks/model.Rmd", output_dir = "docs", output_file = "model.html")
+
+# Generate dashboard
+shinylive::export("dashboard", "docs")
